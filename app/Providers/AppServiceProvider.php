ee8bd581b6fd829acde2405944952133af462a52
Fafi03 <138723875+Fafi03@users.noreply.github.com>
2024-06-05 01:02:05 +0300

    install pusher and laravel echo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,16 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\AuthServiceProvider;
+use Illuminate\Broadcasting\BroadcastServiceProvider;
+use Illuminate\Events\EventServiceProvider;
+use Illuminate\Foundation\Support\Providers\RouteSserviceProvider;
 use Illuminate\Support\ServiceProvider;
+
+class AppAuthServiceProvider extends AuthServiceProvider{}
+class AppBroadcastServiceProvider extends BroadcastServiceProvider{}
+class AppEventServiceProvider extends EventServiceProvider{}
+class AppRouteServiceProvider extends RouteServiceProvider{}
 
 class AppServiceProvider extends ServiceProvider
 {
