d02a7dd6a835917d948c9d32ebccdc8c0e2da5ab
Fafi03 <138723875+Fafi03@users.noreply.github.com>
2024-06-23 01:21:15 +0300

    create model and migrations

---
+++ app/Models/Conversation.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Conversation extends Model
+{
+    use HasFactory;
+
+    protected $guarded=[];
+
+
+    function messages() : HasMany {
+
+        return $this->hasMany(Message::class);
+        
+    }
+
+    function getReceiver()  {
+
+        if($this->sender_id===auth()->id()){
+
+            return User::firstWhere('id',$this->receiver_id);
+        }
+        else{
+            return User::firstWhere('id',$this->sender_id);
+
+        }
+        
+    }
+}
